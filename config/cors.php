287259c4433e693ebcea491fcc0e8144dbe3af5e
MAC <mac@192.168.1.85>
2026-09-18 17:47:23 +0100

    fix cors production vercel

--- config/cors.php
+++ config/cors.php
@@ -1,25 +1,27 @@
 <?php
 
+<?php
+
 return [
 
     /*
     |--------------------------------------------------------------------------
     | Cross-Origin Resource Sharing (CORS) Configuration
     |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => [
+        'api/*',
+        'sanctum/csrf-cookie',
+    ],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
+        'https://griot-frontend-16i5.vercel.app',
+    ],
 
     'allowed_origins_patterns' => [],
 
