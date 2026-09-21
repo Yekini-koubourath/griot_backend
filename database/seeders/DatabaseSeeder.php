eef6a9e28fa7d47944b4f1f4e939e6f0043b7918
MAC <mac@192.168.1.92>
2026-09-21 11:49:37 +0100

    seeders de la page register

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,14 +12,11 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    $this->call(PlanSeeder::class);
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            PlanSeeder::class,
+            AdminSeeder::class,
         ]);
     }
 }
